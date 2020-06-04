aa2c6d1868682edbfdec9aad9442b3e0d54bfa6e
paulo70 <paulofrontend@gmail.com>
2020-06-04 12:24:43 -0300

    build tabs compoments and integrate with redux

--- frontend/src/billingCycle/billingCycle.js
+++ frontend/src/billingCycle/billingCycle.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default props => (
-  <div>
-    <h1>Ciclo de pagamento</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabHeader from '../common/tab/tabHeader'
+import TabsContent from '../common/tab/tabsContent'
+
+class BillingCycle extends Component {
+
+  render(){
+    return (
+      <div>
+        <ContentHeader title='Ciclos de pagamento' small='cadastro' />
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label='Listar'  icon='bars'    target='tabList'  />
+              <TabHeader label='Incluir' icon='plus'    target='tabCreate'/>
+              <TabHeader label='Alterar' icon='pencil'  target='tabUpdate' />
+              <TabHeader label='Excluir' icon='trash-o' target='Excluir'   />
+            </TabsHeader>
+            <TabsContent>
+            </TabsContent>
+          </Tabs>
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default BillingCycle
