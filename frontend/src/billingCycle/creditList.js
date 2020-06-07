74b74fc7fcb26f80815f2a634f7b7cf517d58dfb
paulo70 <paulofrontend@gmail.com>
2020-06-07 14:59:30 -0300

    add method revome to credit list

--- frontend/src/billingCycle/creditList.js
+++ frontend/src/billingCycle/creditList.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
-import {Field, arrayInsert} from 'redux-form'
+import {Field, arrayInsert, arrayRemove} from 'redux-form'
 
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
@@ -11,8 +11,13 @@ class CreditList extends Component {
 
   add(index, item ={}) {
     if(!this.props.readOnly){
-      console.log(index)
       this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+    }
+  }
+
+  remove(index){
+    if(!this.props.readOnly && this.props.list.length > 1){
+      this.props.arrayRemove('billingCycleForm', 'credits', index)
     }
   }
 
@@ -49,6 +54,13 @@ class CreditList extends Component {
             >
             <i className='fa fa-clone'></i>
           </button>
+
+          <button
+            className='btn btn-danger'
+            onClick = {() => this.remove(index)}
+            >
+            <i className='fa fa-trash-o'></i>
+          </button>
         </td>
       </tr>
 
@@ -78,5 +90,5 @@ class CreditList extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert, arrayRemove}, dispatch)
 export default connect(null, mapDispatchToProps)(CreditList)
